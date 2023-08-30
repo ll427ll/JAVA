15d92fa9291e09b41a3fbd5bd971e50eea240b48
kimminseo <gimg51747gmail.com>
2023-08-30 16:31:33 +0900

    init commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void main(String[] args) {
+            System.out.println("Hello java");
+    }
+}
+///* 클래스 블록 */
+//public class 클래스명 {
+//
+//    /* 메서드 블록 */
+//    [public|private|protected] [static] (리턴자료형|void) 메서드명1(입력자료형 매개변수, ...) {
+//        명령문(statement);
+//        ...
+//    }
+//
+//    /* 메서드 블록 */
+//    [public|private|protected] [static] (리턴자료형|void) 메서드명2(입력자료형 매개변수, ...) {
+//        명령문(statement);
+//        ...
+//    }
+//
+//    ...
+//}
